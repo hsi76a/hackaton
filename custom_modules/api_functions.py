76488d2234b993ec385af1284628db0cc7a1f83a
Alex <alexander.de.bruijn@gmail.com>
2022-02-24 14:15:14 +0100

    werkende mvp

--- custom_modules/api_functions.py
+++ custom_modules/api_functions.py
@@ -21,16 +21,44 @@ import config
 
 
 def get_weer_op_locatie(lattitude, longitude):
-    # api request takes 4 parameters:
-    # - longitude and latitude
-    # - api key
-    # -part, dit is welke deel-databases je wilt niet wilt raadplegen, denk daarbij aan houerly, 3 hoouerly, 5 day forcast, zie apidocs
-    # https://openweathermap.org/api/one-call-api
     part = "minutely,hourly,daily"
     resp = requests.get(
-        f"# https://api.openweathermap.org/data/2.5/onecall?lat={lattitude}&lon={longitude}&exclude={part}&units=metric&appid={config.ows_api_key}")
+        f"https://api.openweathermap.org/data/2.5/onecall?lat={lattitude}&lon={longitude}&exclude={part}&units=metric&appid={config.ows_api_key}")
     resp_list = resp.json()
 
     if len(resp_list) == 0:
         return False
     return resp_list
+
+
+def get_verkeers_info(plaatsnaam):
+    url = "https://api.rwsverkeersinfo.nl/api/weatheralerts/"
+    response = requests.request("GET", url)
+    list_weercode = response.json()
+    list_weercode['results'][0]['code']
+
+    url = "https://api.rwsverkeersinfo.nl/api/obstructions/"
+    response = requests.request("GET", url)
+    list_wegobstructie = response.json()
+    items_wegobstructie = ['directionText', 'isCurrent',
+                           'roadNumber', 'timeStart', 'timeEnd']
+    wegobstuctie_weercode_list = []
+    for item in list_wegobstructie['results']:
+        if plaatsnaam in item["directionText"] and item["isCurrent"] == True:
+            templist = []
+            for item_nested in items_wegobstructie:
+                try:
+                    templist.append(item[item_nested])
+
+                except KeyError:
+                    continue
+            templist.append(list_weercode['results'][0]['code'])
+            wegobstuctie_weercode_list.append(templist)
+
+    if len(wegobstuctie_weercode_list) == 0 and list_weercode['results'][0]['code'] >= "":
+        boodschap = f"Geen afsluitingen, let op KNMI geeft weercode {list_weercode['results'][0]['code']} af !!!"
+    elif len(wegobstuctie_weercode_list) == 0:
+        boodschap = "Geen afsluitingen, of weercode"
+    else:
+        boodschap = ""
+    return (boodschap, wegobstuctie_weercode_list)
--- weatherapp.py
+++ weatherapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, render_template, request
-from custom_modules.api_functions import get_weer_op_locatie
+from custom_modules.api_functions import get_weer_op_locatie, get_verkeers_info
 
 app = Flask(__name__)
 
@@ -8,15 +8,33 @@ app = Flask(__name__)
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        lattitude = request.form.get("inputLat")
-        longitude = request.form.get("inputLng")
+        lattitude = request.form.get("lattitude")
+        longitude = request.form.get("longitude")
+        plaatsnaam = request.form.get("plaatsnaam")
         # voer api request uit
         weer_op_locatie = get_weer_op_locatie(lattitude, longitude)
-        if weer_op_locatie:
-            return render_template('index.html', message=f"no Response for weather")
-        else:
-            # pass variabele naar rendertamplate
-            return render_template('index.html', weer_lijst=weer_op_locatie)
+        verkeerinfo = get_verkeers_info(plaatsnaam)
+        # if weer_op_locatie = False:
+        #     return render_template('index.html', message=f"no Response for weather")
+        # else:
+        # pass variabele naar rendertamplate
+
+        wind_deg = weer_op_locatie['current']['wind_deg']
+        windrichting = ""
+
+        if wind_deg > 45:
+            if wind_deg > 135:
+                if wind_deg > 225:
+                    if wind_deg > 315:
+                        windrichting = 'Noord'
+                    else:
+                        windrichting = 'West'
+                else:
+                    windrichting = 'Zuid'
+            else:
+                windrichting = 'Oost'
+
+            return render_template('index.html', wind_richting=windrichting, weer_lijst=weer_op_locatie, verkeerinfo=verkeerinfo)
     else:
         return render_template('index.html')
 
